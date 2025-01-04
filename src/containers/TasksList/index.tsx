0d55a4550519e06e3a267ab675fc1066869b2bd7
maxh33 <contact@maxhaider.dev>
2025-01-03 22:12:19 -0300

    feat: Implement task filtering and update CardFilter component

--- src/containers/TasksList/index.tsx
+++ src/containers/TasksList/index.tsx
@@ -6,16 +6,35 @@ import { RootReducer } from '../../store'
 
 const TasksList = () => {
   const { items } = useSelector((state: RootReducer) => state.tasks)
-  const { term } = useSelector((state: RootReducer) => state.filter)
+  const { term, criteria, value } = useSelector(
+    (state: RootReducer) => state.filter
+  )
 
   const tasksFilter = () => {
-    return items.filter(
-      (item) => item.title.toLowerCase().search(term.toLowerCase()) >= 0
-    )
+    let filerdTasks = items
+    if (term !== undefined) {
+      filerdTasks = filerdTasks.filter(
+        (item) => item.title.toLowerCase().search(term.toLowerCase()) >= 0
+      )
+      if (criteria === 'priority') {
+        filerdTasks = filerdTasks.filter((item) => item.priority === value)
+      } else if (criteria === 'status') {
+        filerdTasks = filerdTasks.filter((item) => item.status === value)
+      }
+      return filerdTasks
+    } else {
+      return items
+    }
   }
+
   return (
     <Container>
       <p>2 tasks marked as: &quot;category&quot; e &quot;{term}&quot;</p>
+      <ul>
+        <li>{term}</li>
+        <li>{criteria}</li>
+        <li>{value}</li>
+      </ul>
       <ul>
         {tasksFilter().map((t) => (
           <li key={t.title}>
